efc7a40631c079bbce888afbb3d571dfbb592cfc
Dip Rawal <diprawal@hotmail.com>
2021-04-22 15:10:13 +0200

    Update archiver.js

--- utils/archiver.js
+++ utils/archiver.js
@@ -14,25 +14,30 @@ async function makeZip() {
   await packagedModules();
   
   const output = fs.createWriteStream(`${branch}.zip`);
+
   return new Promise((resolve) => {
-    output.on(
-      "close",
-      function () {
-        console.log(archive.pointer() + " total bytes");
-        console.log(
-          "archiver has been finalized and the output file descriptor has closed."
-        );
-      },
-      archive.pipe(output),
-      archive.directory(
-        path.join(__dirname, "../candidate-test-nodejs-2021"),
-        false
-      ),
-      archive.directory("subdir/", "new-subdir"),
-      archive.finalize()
-    );
+    resolve(
+      output.on(
+        "close",
+        function () {
+          console.log(archive.pointer() + " total bytes");
+          console.log(
+            "archiver has been finalized and the output file descriptor has closed."
+          );
+        },
+        archive.pipe(output),
+        archive.directory(
+          path.join(__dirname, "../candidate-test-nodejs-2021"),
+          false
+        ),
+        archive.directory("subdir/", "new-subdir"),
+        archive.finalize()
+      )
+    )
+ 
   });
 
 }
 
 module.exports = makeZip;
+
